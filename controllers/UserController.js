29ccf57112b86850dfb52d40a4fffdaea55ab735
OvikGukeyan <105244612+OvikGukeyan@users.noreply.github.com>
2023-03-07 12:16:01 +0300

    back-refactoring

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,14 +1,10 @@
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
-import { validationResult } from 'express-validator';
 import userModel from '../models/User.js'
 
 export const register = async (req, res) => {
     try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json(errors.array())
-        }
+        
 
         const password = req.body.password;
         const solt = await bcrypt.genSalt(10);
