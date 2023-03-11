315af5c916da7f858496f2833536a30d8b224997
OvikGukeyan <105244612+OvikGukeyan@users.noreply.github.com>
2023-03-11 19:49:47 +0300

    fix-login-validation

--- validations.js
+++ validations.js
@@ -2,9 +2,7 @@ import { body } from 'express-validator';
 
 export const loginValidation = [
     body('email').isEmail(),
-    body('password').isLength({min: 5}),
-    body('fullName').isLength({min: 3}),
-    body('avatarUrl').optional().isURL()
+    body('password').isLength({min: 5})
 ]
 
 export const registerValidation = [
@@ -17,6 +15,6 @@ export const registerValidation = [
 export const postCreateValidation = [
     body('title', 'Enter article title').isLength({ min: 3 }).isString(),
     body('text', 'Enter article text').isLength({ min: 3 }).isString(),
-    body('tags', 'Wrong tag format').optional().isString(),
+    body('tags', 'Wrong tag format').optional().isArray(),
     body('imageUrl', 'Invalid image link').optional().isString(),
   ];
