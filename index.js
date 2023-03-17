6a8b6674f1494c8745c00d55602ec6cc11fe4316
OvikGukeyan <105244612+OvikGukeyan@users.noreply.github.com>
2023-03-18 00:28:34 +0300

    debug

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import {UserController, PostController} from './controllers/index.js';
 import {handleValidationErrors, checkAuth} from './utils/index.js';
 
 mongoose
-    .connect(process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb+srv://admin:cjh0RTbuioCIBQ3f@cluster0.dbo7krm.mongodb.net/blog?retryWrites=true&w=majority')
+    .connect(process.env.MONGODB_URI)
     .then(() => { console.log('DB ok') })
     .catch((err) => { console.log('DB Error', err) })
 
